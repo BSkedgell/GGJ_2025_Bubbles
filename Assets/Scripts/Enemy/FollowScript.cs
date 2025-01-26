4fd505e6176d3bbcbdf6250130a4baddc0e73298
Bryan Skedgell <BSkedgell@gmail.com>
2025-01-26 09:24:31 -0600

    Spawner + enemy AI fix
    Updated enemy follow script

--- Assets/Scripts/Enemy/FollowScript.cs
+++ Assets/Scripts/Enemy/FollowScript.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class FollowScript : MonoBehaviour
-
 {
-public NavMeshAgent enemy;
-public Transform player;
+    public NavMeshAgent navMeshAgent; // Reference to the NavMeshAgent component
+    private Transform playerTransform; // Transform of the player
+
+    void Start()
+    {
+        // Find the player object using the "Player" tag
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObject != null)
+        {
+            // Get the player's Transform
+            playerTransform = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("No object with the 'Player' tag found in the scene.");
+        }
+    }
 
     void Update()
     {
-        enemy.SetDestination(player.position);
+        // If the player's transform is found, update the destination of the NavMeshAgent
+        if (playerTransform != null)
+        {
+            navMeshAgent.SetDestination(playerTransform.position);
+        }
     }
 }
